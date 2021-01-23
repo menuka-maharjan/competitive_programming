f92117fc5599ed14ea3ed0db1b34c576d9f2f2c1
menuka-maharjan <maharjanmenuka8@gmail.com>
2021-01-23 22:07:23 +0530

    Create singleNumber2.py

---
+++ bitmanipulation/singleNumber2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        c=Counter(nums)
+        for k,v in c.items():
+            if v==1:
+                return k
+        
+        
+        
+        
