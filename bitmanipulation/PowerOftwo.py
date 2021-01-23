0c214648a3fb1c0956a2a3c1a7b5966a8f1c06d2
menuka-maharjan <maharjanmenuka8@gmail.com>
2021-01-23 22:15:12 +0530

    Create PowerOftwo.py

---
+++ bitmanipulation/PowerOftwo.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while(n):
+            if(n&(n-1)==0):
+                return True
+            else:
+                return False
