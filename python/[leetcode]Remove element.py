e2c2af57b2998c44b9b5a82ffeec703fd2e33b0d
menuka-maharjan <maharjanmenuka8@gmail.com>
2021-01-29 14:45:20 +0530

    modifying folders

--- python/[leetcode]Remove element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        i=0
-        while(i<len(nums)):
-            if(nums[i]==val):
-                del nums[i]
-            else:
-                 i+=1
-        return len(nums)
--- python/[leetcode]median of two sorted arrays.py
+++
@@ -1,5 +0,0 @@
-import statistics
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1=nums1+nums2
-        return statistics.median(nums1)
