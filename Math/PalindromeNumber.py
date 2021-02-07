f58f7fa36cb67c508a7e0e30845cc88d95a8fb47
menuka-maharjan <maharjanmenuka8@gmail.com>
2021-02-07 14:15:34 +0530

    palindrome number problem

---
+++ Math/PalindromeNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        y=str(x)
+        z=y[::-1]
+        if str(x)==z:
+            return True
+        else:
+            return False
